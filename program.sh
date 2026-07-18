2ec18b31cde4700dc68519381f2be8c40a074065
Luciftias <parhamseyfi@icloud.com>
2026-07-17 21:03:54 -0400

    applying option verification check by user and max num of wrong guess

--- program.sh
+++ program.sh
@@ -43,3 +43,37 @@ echo "Each wrong guess adds a piece to the hangman figure!"
 tput setaf 5
 echo "==================================="
 
+
+PLAY="Y"
+# This loop continues until the user says
+# they don't want to play again
+while [[ "$PLAY" == "Y" || "$PLAY" == "y" || "$PLAY" == "YES" ||
+     "$PLAY" == "Yes" || "$PLAY" == "yes" ]]
+do
+    # Ask for the word length (must be a number, 5 or more)
+    LENGTH=0
+    until [ "$LENGTH" -ge 5 ] 2>/dev/null
+    do
+        tput setaf 15   # White text
+        read -p "Word length (minimum 5): " LENGTH
+        if ! [ "$LENGTH" -ge 5 ] 2>/dev/null
+        then
+            tput setaf 1    # Red text
+            echo "Please enter a number that is 5 or more."
+        fi
+    done
+
+    # Ask for the maximum number of wrong guesses (1 or more)
+    MAX=0
+    until [ "$MAX" -ge 1 ] 2>/dev/null
+    do
+        tput setaf 15   # White text
+        read -p "Maximum wrong guesses (minimum 1): " MAX
+        if ! [ "$MAX" -ge 1 ] 2>/dev/null
+        then
+            tput setaf 1    # Red text
+            echo "Please enter a number that is 1 or more."
+        fi
+    done
+
+
